373f321463d731aac66626cbfff6a4da7e41439e
Haluey <cheong7394wol@gmail.com>
2026-07-01 17:24:49 +0900

    비즈니스앱 개발 6일차 커밋

---
+++ toyproject/ToyProjects01/WpfCctvMonitorApp/Services/ItsCctvService.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using WpfCctvMonitorApp.Models;
+
+namespace WpfCctvMonitorApp.Services
+{
+    public class ItsCctvService
+    {
+        private readonly HttpClient httpClient = new ();
+
+        // TODO
+        public async Task<CctvResponse> GetCctvListAsync(string apiUrl)
+        {
+            string json = await httpClient.GetStringAsync(apiUrl);
+
+            var result = JsonConvert.DeserializeObject<CctvResponse>(json);
+
+            if (result == null)
+                return new CctvResponse();
+            else
+                return result;
+        }
+
+        public async Task<ObservableCollection<CctvInfo>> GetAllCctvAsync()
+        {
+            return null;
+        }
+
+    }
+}
