b390d0e54f39ee90fafa1485d36891bdd2485367
Haluey <cheong7394wol@gmail.com>
2026-07-13 17:31:56 +0900

    MVVM개발 1일차

---
+++ toyproject/ToyProjects02/WpfMvvm02/Models/Division.cs
@@ -0,0 +1,7 @@
+﻿namespace WpfMvvm02.Models {
+    public class Division {
+        public string DivCode { get; set; } = string.Empty;
+
+        public string DivName { get; set; } = string.Empty;
+    }
+}
